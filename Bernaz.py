3dbece834cc75c53ce5777971d5cdbfb6ece75fb
Melstolit <melster19@hotmail.com>
2020-04-15 21:48:49 -0400

    Added matplotlib result Used qiskit.visualization inside matplotlib

--- Bernaz.py
+++ Bernaz.py
@@ -1,7 +1,9 @@
 # Importing standard Qiskit libraries and configuring account
 from qiskit import QuantumCircuit, execute, Aer #, IBMQ
 from qiskit.compiler import transpile, assemble
+from qiskit.visualization import plot_histogram, plot_state_city, plot_bloch_vector
 import numpy as np
+import matplotlib.pyplot as plt
 
 circ = QuantumCircuit(4)
 circ.h(0)
@@ -25,22 +27,25 @@ backend = Aer.get_backend('statevector_simulator')
 job = execute(circ, backend)
 result = job.result()
 outputstate = result.get_statevector(circ, decimals=3)
-print(outputstate)
 
-plot_state_city(outputstate)
+#print(outputstate)
+plt.show(plot_state_city(outputstate))
+
 meas = QuantumCircuit(4, 4)
 meas.barrier(range(4))
 meas.measure(range(4),range(4))
 qc = circ+meas
-# qc.draw()
-print(qc)
+plt.show(qc.draw(output='mpl'))
+# print(qc)
 
 backend_sim = Aer.get_backend('qasm_simulator')
 job_sim = execute(qc, backend_sim, shots=1024)
 
 result_sim = job_sim.result()
 counts = result_sim.get_counts(qc)
-print(counts)
 
-plot_histogram(counts)
+#print(counts)
+plt.show(plot_histogram(counts))
 
+bloch = [1, 0, 0]
+plt.show(plot_bloch_vector(bloch))
